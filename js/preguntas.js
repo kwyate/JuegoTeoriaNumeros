f49825f40b72848079ef68aa49379fc93c7ac451
Kewuin Asus <kwyate21@gmail.com>
2020-05-26 09:53:31 -0500

    Se terminan preguntas

--- js/preguntas.js
+++ js/preguntas.js
@@ -7,53 +7,53 @@
 	let oportunidad = 1
 	let preguntas = [
 		{
-			texto: "Como se expresa el numero 18243,18<sub>10</sub> en base binario:",
-			answer: ["100011100011, ####", "1100110101", "100010011100", "101010110101"],
+			texto: "Como se expresa el numero 18243,064<sub>10</sub> en base binario:",
+			answer: ["100011100011,0001101<sub>2</sub>", "100011100011,0101101<sub>8</sub>", "100010011100,0101101<sub>2</sub>", "101010110101,0001111<sub>10</sub>"],
 			correct: 0
 		},
 		{
-			texto: "como se expresa el numero 29042020,1821<sub>10</sub> en base octagonal :",
-			answer: ["Respuesta 5", "Respuesta 6", "Respuesta 7", "Respuesta 8"],
-			correct: 2
+			texto: "como se expresa el numero 29042020<sub>10</sub> en base octagonal :",
+			answer: ["1101110110010010101100100<sub>16</sub>", "11011101100110010101101100<sub>8</sub>", "1101110110010010101100100<sub>8</sub>", "11011101100110010101101100<sub>16</sub>"],
+			correct: 1
 		},
 		{
 			texto: "como se expresa el numero 18032005,45 <sub>10</sub> en base hexadecimal:",
-			answer: ["Respuesta 9", "Respuesta 10", "Respuesta 11", "Respuesta 12"],
+			answer: ["1132575,011100<sub>16</sub>", "1132E85,011100<sub>16</sub>", "1A13E85,011100<sub>16</sub>", "1132585,011100<sub>16</sub>"],
 			correct: 3
 		},
 		{
-			texto: "como se expresa el numero 18212904,50 <sub>10</sub> en base binaria :",
-			answer: ["Respuesta 13", "Respuesta 14", "Respuesta 15", "Respuesta 16"],
-			correct: 3
+			texto: "como se expresa el numero 17212504,50 <sub>8</sub> en base binaria :",
+			answer: ["1111010001010101010101<sub>2</sub>", "1101010001000101000100<sub>2</sub>", "1111010001010101000100<sub>2</sub>", "1111010111010101000100<sub>10</sub>"],
+			correct: 2
 		},
 		{
 			texto: "como se expresa el numero 1001001011001101010001<sub>2</sub> a base decimal :",
-			answer: ["Respuesta 13", "Respuesta 14", "Respuesta 15", "Respuesta 16"],
-			correct: 3
+			answer: ["2405201<sub>10</sub>", "2404201<sub>10</sub>", "2405301<sub>10</sub>", "	2405200<sub>10</sub>"],
+			correct: 0
 		},
 		{
 			texto: "como se expresa el numero 613E3E<sub>16</sub> en base decimal :",
-			answer: ["Respuesta 13", "Respuesta 14", "Respuesta 15", "Respuesta 16"],
-			correct: 3
+			answer: ["6372826<sub>10</sub>", "6372926<sub>10</sub>", "6372726<sub>10</sub>", "6372426<sub>10</sub>"],
+			correct: 1
 		},
 		{
 			texto: "como se expresa el numero 54A27G<sub>16</sub> a base binaria :",
-			answer: ["Respuesta 13", "Respuesta 14", "Respuesta 15", "Respuesta 16"],
+			answer: ["1010101101000100111<sub>2</sub> ", "1011100101000100111<sub>2</sub> ", "1010100101000100101<sub>2</sub> ", "1010100101000100111<sub>2</sub> "],
 			correct: 3
 		},
 		{
 			texto: "como se expresa el numero 1563461<sub>8</sub> en base binaria :",
-			answer: ["Respuesta 13", "Respuesta 14", "1101110011100110001", "Respuesta 16"],
+			answer: ["1101110010100110001<sub>2</sub>", "110111001110001<sub>2</sub>", "1101110011100110001<sub>2</sub>", "1101110011100110101<sub>2</sub>"],
 			correct: 2
 		},
 		{
 			texto: "como se expresa el numero 517806<sub>10</sub> a base octal y base binaria :",
-			answer: ["1763256 y 1111110011010101110", "Respuesta 14", "Respuesta 15", "Respuesta 16"],
+			answer: ["1763256<sub>8</sub> y 1111110011010101110<sub>2</sub>", "1762256<sub>8</sub> y 1111110011011101110<sub>2</sub>", "1763156<sub>8</sub> y 1110010011010101110<sub>2</sub>", "1863256<sub>8</sub> y 111111001101010210<sub>2</sub>"],
 			correct: 0
 		},
 		{
 			texto: "como se expresa el numero 110010101110011111101001<sub>2</sub> a base  octal :",
-			answer: ["Respuesta 13", "Respuesta 14", "Respuesta 15", "62563751"],
+			answer: ["62563750<sub>8</sub>", "62562751<sub>8</sub>", "63563751<sub>8</sub>", "62563751<sub>8</sub>"],
 			correct: 3
 		}
 	]
@@ -101,7 +101,7 @@ const questions = () =>{
 		document.querySelector(".texto").innerHTML = preguntas[numquestion].texto;
 		circle_question[numquestion].classList.add("active")
 		box_answer.map(( element, index)=>{
-			element.childNodes[1].innerText = preguntas[numquestion].answer[index];
+			element.childNodes[1].innerHTML = preguntas[numquestion].answer[index];
 			if (index == preguntas[numquestion].correct) {
 				element.childNodes[3].setAttribute("correct", true);
 			}else{
@@ -128,7 +128,7 @@ const questions = () =>{
 				}else if ( element.classList.contains("incorrect")){
 					element.classList.remove("incorrect");
 				}
-				element.childNodes[1].innerText = preguntas[numquestion].answer[index];
+				element.childNodes[1].innerHTML = preguntas[numquestion].answer[index];
 				if (index == preguntas[numquestion].correct) {
 					element.childNodes[3].setAttribute("correct", true);
 				}else{
